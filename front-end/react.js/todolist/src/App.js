62c6ab06eb528d81b0fa36999316b111795afc5f
jaismile14 <jaismile14@gmail.com>
2022-07-08 14:53:17 -0400

    Initial Commit

---
+++ front-end/react.js/todolist/src/App.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import logo from "./logo.svg";
+import CounterWithNameAndSideEffect from "./CounterWithNameAndSideEffect";
+import SearchFilterDemo from "./SearchFilterDemo";
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+import Welcome from "./Welcome";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: true },
+    { id: 3, text: "Take shower", done: false },
+  ]);
+
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo"></img>
+
+      <Routes>
+        <Route exact path="/" element={<Welcome user={"Akash"} />} />
+
+        <Route exact path="/search" element={<SearchFilterDemo />} />
+
+        <Route
+          exact
+          path="/counter"
+          element={
+            <>
+              <p>You clicked {count} times</p>
+              <button onClick={() => setCount(count + 1)}>Click me</button>
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/counterWithNameAndSideEffect"
+          element={<CounterWithNameAndSideEffect user={"Akash Das"} />}
+        />
+
+        <Route
+          exact
+          path="/todos"
+          element={
+            <>
+              <TodoList todos={todos} setTodos={setTodos} />
+              <AddTodo setTodos={setTodos} />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+// display a h1 with "Hi", if the message is equal to "Hello",
+// else print "Sorry, I'm busy." in h4 and red colour
